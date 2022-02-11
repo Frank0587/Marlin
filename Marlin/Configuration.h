27ab250aca8c3f0a86c1e506d9c38c9eb22fb2f8
Frank0587 <bhunjimko@web.de>
2022-02-11 18:34:02 +0100

    clean Version Infos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,8 @@
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "SP# (JyersUI)" // Who made the changes.
-#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+
+//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 
 
@@ -156,7 +157,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 V2"
+#define CUSTOM_MACHINE_NAME "Ender3v2+Mod#2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
