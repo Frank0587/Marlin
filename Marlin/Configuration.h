7c19eb923ff21e333b60402a81faee355ae3236c
Frank0587 <bhunjimko@web.de>
2022-02-14 12:40:01 +0100

    get debug-infos from SP#xx

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -85,6 +85,7 @@
 //#define DEBUG_POWER_LOSS_RECOVERY   // M1000
 #define DEBUG_PAUSE_RESUME    // Pause, M25, M125, M600, ...
 #define DEBUG_LCD_UI          // LCD dwinui, dwin_lcd, marlinui...
+// #define DEBUG_ONSCREEN        // add infolines on LCD-screen
 
 /**
  * *** VENDORS PLEASE READ ***
@@ -2847,7 +2848,7 @@
 // Ender-3 v2 OEM display with enhancements by Jacob Myers
 //
 #define DWIN_CREALITY_LCD_JYERSUI
-  //#define DWIN_CREALITY_LCD_CUSTOM_ICONS
+  #define DWIN_CREALITY_LCD_CUSTOM_ICONS                  // needed DWIN-Firmware IconSet: Custom or Gotscha, not Orginal
   #define DWIN_CREALITY_LCD_JYERSUI_GCODE_PREVIEW
 
 //
--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -35,7 +35,7 @@
  */
 #ifndef DETAILED_BUILD_VERSION
   // = details for info screen 
-  #define DETAILED_BUILD_VERSION "SP +Jy>2.0.2i +Mar>bugfix"
+  #define DETAILED_BUILD_VERSION "SP / Jy / Mar>bugfix"
 #endif
 
 /**
